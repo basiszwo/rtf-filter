071193a89c790ced760d4e2bb33989ebb3141a87
Mohamed Wael Khobalatte <wael.khobalatte@gmail.com>
2015-01-16 16:12:30 +0100

    Ensure program builds on Mac OSX

--- src/main.cpp
+++ src/main.cpp
@@ -27,7 +27,7 @@ int main(int argc, char *argv[]){
     boost::filesystem::path destinationFile("");
     // Configure valid options and get user's options
     try {
-        getOptions( argc, argv, verbose, quiet, toStdOut, sourceFile, destinationFile, allowTags);    	
+        getOptions( argc, argv, verbose, quiet, toStdOut, sourceFile, destinationFile, allowTags);
     }
     catch(...){
         std::cerr << "Error while getting options." << std::endl;
@@ -36,7 +36,7 @@ int main(int argc, char *argv[]){
     // Open RTF input file
     if( verbose && !toStdOut){
       std::cout << "Opening RTF file: " << sourceFile << std::endl;
-    } 
+    }
     fs::ifstream is( sourceFile );
     // Output plain text...
     if( toStdOut ){
@@ -46,10 +46,10 @@ int main(int argc, char *argv[]){
 		}
 		else {
 			std::cout << "Excluding angle brackets in output.  ";
-		}		
+		}
         std::cout << "Outputting to console." << std::endl;
-      }    
-    
+      }
+
       convert( is, std::cout, 0, allowTags, verbose );
     }
     else {
@@ -59,12 +59,12 @@ int main(int argc, char *argv[]){
 		}
 		else {
 			std::cout << "Excluding angle brackets in output.  ";
-		}		
-        std::cout << "Outputting plaintext to file: " << destinationFile.file_string() << std::endl;
+		}
+        std::cout << "Outputting plaintext to file: " << destinationFile.string() << std::endl;
       }
       fs::ofstream os(destinationFile);
       convert( is, os, 0, allowTags, verbose );
     }
 
-    return 0;                                                                                             
-}                                  
+    return 0;
+}
